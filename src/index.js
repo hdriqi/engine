8d1ad588852cb426d481cb6dac66925e2df0b173
hdriqi <ra@evius.id>
2018-09-03 12:50:38 +0700

    add simple cors to main app

--- src/index.js
+++ src/index.js
@@ -75,6 +75,13 @@ class Engine {
 			}
 		}))
 
+		this.app.use((req, res, next) => {
+			res.header('Access-Control-Allow-Origin', '*')
+			res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+			res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+			next()
+		})
+
 		// Routes
 		this.app.use(subdomain('auth', auth.router(this)))
 		this.app.use(subdomain('api', api.router(this)))
