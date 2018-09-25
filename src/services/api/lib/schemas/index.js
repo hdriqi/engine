8e601cd90941c18c69a9578a9ff7bd2a10c27731
hdriqi <ra@evius.id>
2018-09-25 20:10:35 +0700

    remove core schema from users schema

--- src/services/api/lib/schemas/index.js
+++ src/services/api/lib/schemas/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 
+const removeCoreSchemas = (response) => {
+	delete response['CORE_ANALYTICS']
+	delete response['CORE_BANDWIDTHS']
+}
+
 export default (ctx) => {
 	const myRouter = express.Router()
 
 	myRouter.get('/projects/:projectId/schemas', async (req, res) => {
 		try {
 			const response = Object.assign({}, await ctx.utils.schema.get(ctx, req.params.projectId))
-			delete response['CORE_ANALYTICS']
+			removeCoreSchemas(response)
 			res.status(200).json({
 				status: 'success',
 				data: response
@@ -27,7 +32,7 @@ export default (ctx) => {
 				name: req.body.name,
 				desc: req.body.desc
 			}))
-			delete response['CORE_ANALYTICS']
+			removeCoreSchemas(response)
 			res.status(200).json({
 				status: 'success',
 				data: response
@@ -46,7 +51,7 @@ export default (ctx) => {
 		try {
 			const params = Object.assign(req.body, {name: req.params.schemaId})
 			const response = Object.assign({}, await ctx.utils.schema.modify(ctx, req.params.projectId, params))
-			delete response['CORE_ANALYTICS']
+			removeCoreSchemas(response)
 			res.status(200).json({
 				status: 'success',
 				data: response
@@ -64,7 +69,7 @@ export default (ctx) => {
 			const response = Object.assign({}, await ctx.utils.schema.delete(ctx, req.params.projectId, {
 				name: req.params.schemaId
 			}))
-			delete response['CORE_ANALYTICS']
+			removeCoreSchemas(response)
 			res.status(200).json({
 				status: 'success',
 				data: response
